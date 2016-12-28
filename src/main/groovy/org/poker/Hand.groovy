295cbd3cda0228f03238238a73c57d1f43a4919e
vsajja <vinod.sajja@gmail.com>
2016-12-27 22:28:46 -0700

    progress

--- src/main/groovy/org/poker/Hand.groovy
+++ src/main/groovy/org/poker/Hand.groovy
@@ -3,11 +3,16 @@ package org.poker
 import jooq.generated.tables.pojos.Card
 
 class Hand {
+    public static final int REQUIRED_CARDS_PER_HAND = 5
+
     List<Card> cards = []
     HandType handType
 
     public Hand(List<Card> cards) {
         this.cards = cards
+
+        assert cards.size() == REQUIRED_CARDS_PER_HAND
+
         this.handType = evaluate()
     }
 
@@ -26,6 +31,12 @@ class Hand {
 
     public boolean isFourOfAKind() {
         Map ranks = cards.groupBy { Card card -> card.rank }
+        println ranks
         return ranks.findAll { k, v -> v.size() == 4 }.size() == 1
+    }
+
+    @Override
+    String toString() {
+        return cards.collect { it.rankStr + ' of ' + it.suit }.toString()
     }
 }
--- src/test/groovy/org/poker/HandSpec.groovy
+++ src/test/groovy/org/poker/HandSpec.groovy
@@ -1,13 +1,82 @@
 package org.poker
 
+import jooq.generated.tables.pojos.Card
 import spock.lang.Specification
+import spock.lang.Unroll
 
 public class HandSpec extends Specification {
     def cleanupSpec() {
 
     }
 
-    def "test"() {
-        expect: false
+    @Unroll
+    def "is four of a kind: #hand"() {
+        expect:
+        hand.isFourOfAKind() == true
+
+        where:
+        hand << [
+                createHand('2clubs 2spades 2hearts 2diamonds 3clubs'),
+                createHand('3clubs 3spades 3hearts 3diamonds 9clubs'),
+                createHand('aceclubs acespades acehearts acediamonds jackclubs')]
+    }
+
+    @Unroll
+    def "is not four of a kind: #hand"() {
+        expect:
+        hand.isFourOfAKind() == false
+
+        where:
+        hand << [
+                createHand('2clubs 2spades 2hearts 3diamonds 3clubs'),
+                createHand('2clubs 3spades 4hearts 5diamonds 6clubs'),
+                createHand('aceclubs acespades acehearts jackdiamonds jackclubs')]
+    }
+
+    /**
+     * Creates a 5-card poker hand. @see {@link org.poker.Hand}
+     * @param handStr
+     * example: '2clubs 2spades 2hearts 2diamonds 3clubs'
+     */
+    Hand createHand(handStr) {
+        Map suits = [ 'C' : 'clubs',
+                      'S' : 'spades',
+                      'H' : 'hearts',
+                      'D' : 'diamonds']
+        Map ranks = [
+                '2' : 2,
+                '3' : 3,
+                '4' : 4,
+                '5' : 5,
+                '6' : 6,
+                '7' : 7,
+                '8' : 8,
+                '9' : 9,
+                '10': 10,
+                'jack' : 11,
+                'queen' : 12,
+                'king' : 13,
+                'ace' : 14
+        ]
+
+        List<Card> cards = []
+
+        if(handStr) {
+            def cardArray = handStr.split(' ')
+            cardArray.each { String cardStr ->
+                suits.each { String suit ->
+                    if(cardStr.contains(suit)) {
+                        def rankStr = cardStr - suit
+                        def name = rankStr + '_of_' + suit
+
+                        Card card = new Card(null, name, ranks[rankStr.toLowerCase()], rankStr, suit, null)
+                        cards.add(card)
+                    }
+                }
+            }
+        }
+
+        Hand hand = new Hand(cards)
+        return hand
     }
 }
