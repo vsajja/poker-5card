3e1b9a846f3678ce7191c09e170e82b5ecaf370e
vsajja <vinod.sajja@gmail.com>
2016-12-28 02:09:25 -0700

    progress

--- src/main/groovy/org/poker/Hand.groovy
+++ src/main/groovy/org/poker/Hand.groovy
@@ -8,6 +8,9 @@ class Hand {
     List<Card> cards = []
     HandType handType
 
+    Map ranks = [:]
+    Map suits = [:]
+
     public Hand(List<Card> cards) {
         this.cards = cards
 
@@ -17,22 +20,74 @@ class Hand {
     }
 
     HandType evaluate() {
+        ranks = cards.groupBy { Card card -> card.rank }
+        suits = cards.groupBy { Card card -> card.suit }
+
         return HandType.STRAIGHT_FLUSH
     }
 
-    public boolean isStraight() {
-
+    protected boolean isPair() {
+        return (ranks.findAll { k, v -> v.size() == 2 }.size() == 1) && !isFullHouse()
     }
 
-    public boolean isFlush() {
-        Map suits = cards.groupBy { Card card -> card.suit }
-        return suits.findAll { k, v -> v.size() == 5 }.size() == 1
+    protected boolean isTwoPair() {
+        return ranks.findAll { k, v -> v.size() == 2 }.size() == 2
     }
 
-    public boolean isFourOfAKind() {
-        Map ranks = cards.groupBy { Card card -> card.rank }
-        println ranks
+    protected boolean isThreeOfAKind() {
+        return (ranks.findAll { k, v -> v.size() == 3 }.size() == 1) && !isFullHouse()
+    }
+
+    protected boolean isStraight() {
+        def values = cards.collect { it.rank }
+        return isSequential(values) && (suits.findAll { k, v -> v.size() == 5 }.size() == 0)
+    }
+
+    boolean isSequential(List<Integer> values) {
+        boolean isStraight = true
+
+        int ACE_RANK_HIGH = 14
+        int ACE_RANK_LOW = 1
+
+        if (values.contains(ACE_RANK_HIGH) && values.containsAll([2, 3, 4, 5])) {
+            values.remove((values.indexOf(ACE_RANK_HIGH)))
+            values.add(ACE_RANK_LOW)
+        }
+
+        values.sort()
+
+        for (int i = 0; i < values.size(); i++) {
+            def nextValue = values[i + 1]
+            def currentValue = values[i]
+
+            if (nextValue) {
+                if (nextValue != currentValue + 1) {
+                    isStraight = false
+                }
+            }
+        }
+        return isStraight
+    }
+
+    protected boolean isFlush() {
+        def values = cards.collect { it.rank }
+        return !isSequential(values) && (suits.findAll { k, v -> v.size() == 5 }.size() == 1)
+    }
+
+    protected boolean isFullHouse() {
+        def hasThreeOfAKind = ranks.findAll { k, v -> v.size() == 3 }.size() == 1
+        def hasOnePair = ranks.findAll { k, v -> v.size() == 2 }.size() == 1
+
+        return hasThreeOfAKind && hasOnePair
+    }
+
+    protected boolean isFourOfAKind() {
         return ranks.findAll { k, v -> v.size() == 4 }.size() == 1
+    }
+
+    protected boolean isStraightFlush() {
+        def values = cards.collect { it.rank }
+        return isSequential(values) && (suits.findAll { k, v -> v.size() == 5 }.size() == 1)
     }
 
     @Override
--- src/test/groovy/org/poker/HandSpec.groovy
+++ src/test/groovy/org/poker/HandSpec.groovy
@@ -4,33 +4,185 @@ import jooq.generated.tables.pojos.Card
 import spock.lang.Specification
 import spock.lang.Unroll
 
+@Unroll
 public class HandSpec extends Specification {
-    def cleanupSpec() {
+    def "pair: #hand"() {
+        expect:
+        hand.isPair() == true
+
+        where:
+        hand << createHands([HandType.PAIR])
+    }
+
+    def "not pair: #hand"() {
+        expect:
+        hand.isPair() == false
+
+        where:
+        hand << createHands((HandType.values() - HandType.PAIR).toList())
+    }
+
+    def "two pair: #hand"() {
+        expect:
+        hand.isTwoPair() == true
+
+        where:
+        hand << createHands([HandType.TWO_PAIR])
+    }
+
+    def "not two pair: #hand"() {
+        expect:
+        hand.isTwoPair() == false
+
+        where:
+        hand << createHands((HandType.values() - HandType.TWO_PAIR).toList())
+    }
+
+    def "three of a kind: #hand"() {
+        expect:
+        hand.isThreeOfAKind() == true
+
+        where:
+        hand << createHands([HandType.THREE_OF_A_KIND])
 
     }
 
-    @Unroll
-    def "is four of a kind: #hand"() {
+    def "not three of a kind: #hand"() {
+        expect:
+        hand.isThreeOfAKind() == false
+
+        where:
+        hand << createHands((HandType.values() - HandType.THREE_OF_A_KIND).toList())
+    }
+
+    def "straight: #hand"() {
+        expect:
+        hand.isStraight() == true
+
+        where:
+        hand << createHands([HandType.STRAIGHT])
+    }
+
+    def "not straight: #hand"() {
+        expect:
+        hand.isStraight() == false
+
+        where:
+        hand << createHands((HandType.values() - HandType.STRAIGHT).toList())
+    }
+
+    def "flush: #hand"() {
+        expect:
+        hand.isFlush() == true
+
+        where:
+        hand << createHands([HandType.FLUSH])
+    }
+
+    def "not flush: #hand"() {
+        expect:
+        hand.isFlush() == false
+
+        where:
+        hand << createHands((HandType.values() - HandType.FLUSH).toList())
+    }
+
+    def "full house: #hand"() {
+        expect:
+        hand.isFullHouse() == true
+
+        where:
+        hand << createHands([HandType.FULL_HOUSE])
+    }
+
+    def "not full house: #hand"() {
+        expect:
+        hand.isFullHouse() == false
+
+        where:
+        hand << createHands((HandType.values() - HandType.FULL_HOUSE).toList())
+    }
+
+    def "four of a kind: #hand"() {
         expect:
         hand.isFourOfAKind() == true
 
         where:
-        hand << [
-                createHand('2clubs 2spades 2hearts 2diamonds 3clubs'),
-                createHand('3clubs 3spades 3hearts 3diamonds 9clubs'),
-                createHand('aceclubs acespades acehearts acediamonds jackclubs')]
+        hand << createHands([HandType.FOUR_OF_A_KIND])
     }
 
-    @Unroll
-    def "is not four of a kind: #hand"() {
+    def "not four of a kind: #hand"() {
         expect:
         hand.isFourOfAKind() == false
 
         where:
-        hand << [
-                createHand('2clubs 2spades 2hearts 3diamonds 3clubs'),
-                createHand('2clubs 3spades 4hearts 5diamonds 6clubs'),
-                createHand('aceclubs acespades acehearts jackdiamonds jackclubs')]
+        hand << createHands((HandType.values() - HandType.FOUR_OF_A_KIND).toList())
+    }
+
+    def "straight flush: #hand"() {
+        expect:
+        hand.isStraightFlush() == true
+
+        where:
+        hand << createHands([HandType.STRAIGHT_FLUSH])
+    }
+
+    def "not straight flush: #hand"() {
+        expect:
+        hand.isStraightFlush() == false
+
+        where:
+        hand << createHands((HandType.values() - HandType.STRAIGHT_FLUSH).toList())
+    }
+
+    List<Hand> createHands(List<HandType> types) {
+        List<Hand> hands = []
+
+        types.each { HandType type ->
+            switch (type) {
+                case HandType.HIGH_CARD:
+                    hands << createHand('2clubs 3spades 5hearts 6diamonds 7clubs')
+                    hands << createHand('8spades 9clubs 10diamonds jackdiamonds acehearts')
+                    break;
+                case HandType.PAIR:
+                    hands << createHand('2clubs 2spades 3hearts 4diamonds 5clubs')
+                    hands << createHand('8clubs 9spades 10hearts jackdiamonds jackclubs')
+                    break;
+                case HandType.TWO_PAIR:
+                    hands << createHand('2clubs 2spades 4hearts 4diamonds 5clubs')
+                    hands << createHand('8clubs 10spades 10hearts acediamonds aceclubs')
+                    break;
+                case HandType.THREE_OF_A_KIND:
+                    hands << createHand('2clubs 2spades 2hearts 4diamonds 5clubs')
+                    hands << createHand('8clubs 10spades 10hearts 10diamonds jackclubs')
+                    break;
+                case HandType.STRAIGHT:
+                    hands << createHand('aceclubs 2spades 3hearts 4diamonds 5clubs')
+                    hands << createHand('9clubs 10spades jackhearts queendiamonds kingclubs')
+                    hands << createHand('10clubs jackspades queenhearts kingdiamonds aceclubs')
+                    break;
+                case HandType.FLUSH:
+                    hands << createHand('aceclubs 2clubs 3clubs 4clubs 6clubs')
+                    hands << createHand('2diamonds 6diamonds 9diamonds 4diamonds jackdiamonds')
+                    break;
+                case HandType.FULL_HOUSE:
+                    hands << createHand('2clubs 2spades 2hearts 4diamonds 4clubs')
+                    hands << createHand('jackdiamonds 10spades 10hearts 10diamonds jackclubs')
+                    break;
+                case HandType.FOUR_OF_A_KIND:
+                    hands << createHand('2clubs 2spades 2hearts 2diamonds 4clubs')
+                    hands << createHand('4clubs jackclubs jackspades jackhearts jackdiamonds')
+                    break;
+                case HandType.STRAIGHT_FLUSH:
+                    hands << createHand('aceclubs 2clubs 3clubs 4clubs 5clubs')
+                    hands << createHand('9spades 10spades jackspades queenspades kingspades')
+                    hands << createHand('10diamonds jackdiamonds queendiamonds kingdiamonds acediamonds')
+                    break;
+                default:
+                    break;
+            }
+        }
+        return hands
     }
 
     /**
@@ -39,33 +191,33 @@ public class HandSpec extends Specification {
      * example: '2clubs 2spades 2hearts 2diamonds 3clubs'
      */
     Hand createHand(handStr) {
-        Map suits = [ 'C' : 'clubs',
-                      'S' : 'spades',
-                      'H' : 'hearts',
-                      'D' : 'diamonds']
+        def suits = ['clubs',
+                     'spades',
+                     'hearts',
+                     'diamonds']
         Map ranks = [
-                '2' : 2,
-                '3' : 3,
-                '4' : 4,
-                '5' : 5,
-                '6' : 6,
-                '7' : 7,
-                '8' : 8,
-                '9' : 9,
-                '10': 10,
+                '2'    : 2,
+                '3'    : 3,
+                '4'    : 4,
+                '5'    : 5,
+                '6'    : 6,
+                '7'    : 7,
+                '8'    : 8,
+                '9'    : 9,
+                '10'   : 10,
                 'jack' : 11,
-                'queen' : 12,
+                'queen': 12,
                 'king' : 13,
-                'ace' : 14
+                'ace'  : 14
         ]
 
         List<Card> cards = []
 
-        if(handStr) {
+        if (handStr) {
             def cardArray = handStr.split(' ')
             cardArray.each { String cardStr ->
                 suits.each { String suit ->
-                    if(cardStr.contains(suit)) {
+                    if (cardStr.contains(suit)) {
                         def rankStr = cardStr - suit
                         def name = rankStr + '_of_' + suit
 
